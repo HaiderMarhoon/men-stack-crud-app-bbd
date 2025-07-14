3b169c71286cb6f2f04cf684fec488ba18178dc3
HaiderMarhoon <haidooer2003@gmail.com>
2025-07-14 09:42:42 +0300

    connection to the db

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+const dotenv = require("dotenv").config();
+
 const express = require("express")
+const mongoose = require("mongoose");
 
 const app =express()
 const path = require("path");
+//db connection
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on("connected",()=>{
+    console.log(`Connected to MangoDB ${mongoose.connection.name} `)
+} )
 
 // MIDDLEWARE
 app.use(express.static(path.join(__dirname, "public")));
